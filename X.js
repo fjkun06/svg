0e4a2c93fd5968d0d1da2e07f8c8450c7490cd2c
fjkun06 <fjkun06@gmail.com>
2022-10-26 07:33:23 +0100

    Nested SVGs

---
+++ X.js
@@ -0,0 +1,24 @@
+function name(
+  params,
+  ddddd,
+  ddd,
+  fff,
+  ffff,
+  gggg,
+  ttt,
+  rrrr,
+  eeee,
+  eeee,
+  tttt,
+  yyyy,
+  uuuu,
+  iiii,
+  oooo,
+  dddd,
+  ssss,
+  eeee
+) {
+  console.log(
+    "d"
+  );
+}
